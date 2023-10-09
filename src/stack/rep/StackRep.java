5e50c1f197ea41f16921dc8e734b09ace323bf5b
dyoas123 <rautrautjoash1@gmail.com>
2023-10-08 19:02:36 -0700

    added initial files

---
+++ src/stack/rep/StackRep.java
@@ -0,0 +1,30 @@
+
+package stack.rep;
+
+import java.util.Stack;
+public class StackRep {
+
+    
+    public static void main(String[] args) {
+Stack<String> stack = new Stack<String>();
+
+        System.out.println("my subject for today is :");
+
+stack.push("event driven programming");        
+stack.push("Data structure ang algorithm");
+stack.push("Multimedia Systems Development");
+       
+
+stack.pop();
+
+System.out.println(stack);
+
+
+        
+        
+        
+       
+        }
+    }
+    
+
